3832c58b86660d964407b9de1060f524d624f874
seunzone <darealseun@gmail.com>
2017-08-15 12:36:41 +0100

    Displaying results on the view page

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+app.set("view engine", "ejs");
 
-app.get("/results", function(request, response){
-	res.send("Yaay it works");
+
+
+app.get("/results", function(req, res){
+	request("http://theapache64.xyz:8080/movie_db/search?keyword=blade", function(error, response, body){
+		if(!error && response.statusCode == 200){
+			var data = JSON.parse(body)
+			res.render("results", {data:data});
+		}
+	});
 });
 
 
