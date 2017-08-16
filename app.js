bc397e8b393d60fd6af276af3e988c8001368e74
seunzone <darealseun@gmail.com>
2017-08-16 10:54:29 +0100

    Pulling moving data using API

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+	res.render("search");
+});
+
 
 
 app.get("/results", function(req, res){
-	request("http://theapache64.xyz:8080/movie_db/search?keyword=blade", function(error, response, body){
+	var query = req.query.search;
+	request("http://theapache64.xyz:8080/movie_db/search?keyword=" + query, function(error, response, body){
 		if(!error && response.statusCode == 200){
 			var data = JSON.parse(body)
 			res.render("results", {data:data});
